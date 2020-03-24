00fb914a577b3f8a249c8f9e688efad610b06672
Joslin, Brady W (Brady) <brady@bradyjoslin.com>
2020-03-24 17:34:53 -0500

    refactor

--- src/main.rs
+++ src/main.rs
@@ -14,27 +14,34 @@ fn main() {
     let opt = Opt::from_args();
     let ssid = match opt.ssid {
         Some(ssid_in) => ssid_in.to_owned(),
-        None => {
-            let airport_path =
-            "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport";
-            let output = Command::new(airport_path)
-                .arg("-I")
-                .output()
-                .expect("Airport not found");
-            let airport_info = String::from_utf8(output.stdout).expect("Not UTF-8");
-            airport_info
-                .lines()
-                .filter(|x| x.contains("SSID"))
-                .last()
-                .expect("SSID not found")
-                .split("SSID:")
-                .last()
-                .expect("SSID not found")
-                .trim()
-                .to_owned()
-        }
+        None => connected_ssid(),
     };
+    let password = password_from_keychain(ssid);
 
+    println!("{}", password);
+}
+
+fn connected_ssid() -> String {
+    let airport_path =
+        "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport";
+    let output = Command::new(airport_path)
+        .arg("-I")
+        .output()
+        .expect("Airport not found");
+    let airport_info = String::from_utf8(output.stdout).expect("Not UTF-8");
+    airport_info
+        .lines()
+        .filter(|x| x.contains("SSID"))
+        .last()
+        .expect("SSID not found")
+        .split("SSID:")
+        .last()
+        .expect("SSID not found")
+        .trim()
+        .to_owned()
+}
+
+fn password_from_keychain(ssid: String) -> String {
     let output = Command::new("security")
         .arg("find-generic-password")
         .args(&["-D", "AirPort network password"])
@@ -42,7 +49,7 @@ fn main() {
         .output()
         .expect("process failed to execute");
     let password_raw = String::from_utf8(output.stderr).expect("Not UTF-8");
-    let password = password_raw
+    password_raw
         .lines()
         .last()
         .expect("Password not found")
@@ -50,6 +57,5 @@ fn main() {
         .last()
         .expect("Password not found")
         .trim()
-        .replace("\"", "");
-    println!("{}", password);
+        .replace("\"", "")
 }
