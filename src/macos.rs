fba042cc0aa762c9cd722d8747a30cf87fcbfca7
Brady Joslin <brady@bradyjoslin.com>
2021-03-12 17:11:37 -0600

    update macos to use run_script

--- src/macos.rs
+++ src/macos.rs
@@ -1,17 +1,18 @@
-use std::{io, process::Command};
+use run_script::ScriptOptions;
+use std::io;
 
 use crate::errors::{AppResult, Error};
 use io::Write;
 
 pub fn connected_ssid() -> AppResult<String> {
-    let airport_path =
-        "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport";
-    let output = Command::new(airport_path)
-        .arg("-I")
-        .output()
-        .expect("Airport not found");
-    let airport_info = String::from_utf8(output.stdout).expect("Not UTF-8");
-    let ssid = airport_info
+    let (_, output, _) = run_script::run_script!(
+        r#"
+            /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I
+        "#
+    )
+    .expect("Airport not found");
+
+    let ssid = output
         .lines()
         .filter(|x| x.contains("SSID"))
         .last()
@@ -21,6 +22,7 @@ pub fn connected_ssid() -> AppResult<String> {
         .unwrap_or_default()
         .trim()
         .to_owned();
+
     match ssid.as_str() {
         "" => Err(Error::SSIDMissing),
         _ => Ok(ssid),
@@ -28,27 +30,31 @@ pub fn connected_ssid() -> AppResult<String> {
 }
 
 pub fn get_password(ssid: &str) -> AppResult<String> {
-    let output = Command::new("security")
-        .args(&["find-generic-password", "-w"])
-        .args(&["-D", "AirPort network password"])
-        .args(&["-ga", ssid])
-        .output();
-    match output {
-        Ok(o) => match o.status.code().unwrap() {
-            0 => {
-                let password = String::from_utf8(o.stdout)
-                    .expect("Not UTF-8")
-                    .trim()
-                    .to_owned();
-                match password.as_str() {
-                    "" => Err(Error::PasswordNotFound),
-                    _ => Ok(password),
-                }
+    let options = ScriptOptions::new();
+
+    let (code, output, error) = run_script::run_script!(
+        r#"
+            security find-generic-password -w -D "AirPort network password" -ga "$1"
+        "#,
+        &vec![ssid.to_string()],
+        options
+    )
+    .unwrap();
+
+    if !error.is_empty() {
+        panic!(error)
+    };
+
+    match code {
+        0 => {
+            let password = output.trim().to_owned();
+            match password.as_str() {
+                "" => Err(Error::PasswordNotFound),
+                _ => Ok(password),
             }
-            44 => Err(Error::SSIDNotFound),
-            _ => Err(Error::KeychainAccess),
-        },
-        Err(e) => panic!(e),
+        }
+        44 => Err(Error::SSIDNotFound),
+        _ => Err(Error::KeychainAccess),
     }
 }
 
@@ -62,23 +68,25 @@ pub fn always_allow(ssid: &str) -> AppResult<()> {
     let input = input.as_str().trim();
 
     if input == "y" {
-        let output = Command::new("security")
-            .args(&["add-generic-password", "-U"])
-            .args(&["-a", ssid])
-            .args(&["-D", "AirPort network password"])
-            .args(&["-T", "/usr/bin/security"])
-            .args(&["-s", "AirPort"])
-            .arg("/Library/Keychains/System.keychain")
-            .output();
-        match output {
-            Ok(o) => match o.status.code().unwrap() {
-                0 => {
-                    println!("Keychain updated...\n");
-                    Ok(())
-                }
-                _ => Err(Error::KeyChainWriteAccess),
-            },
-            Err(e) => panic!(e),
+        let options = ScriptOptions::new();
+
+        let (code, _, _) = run_script::run_script!(
+            r#"
+                security add-generic-password -U -a "$1" \
+                -D "AirPort network password" -T "/usr/bin/security" \
+                -s "AirPort" /Library/Keychains/System.keychain
+            "#,
+            &vec![ssid.to_string()],
+            options
+        )
+        .unwrap();
+
+        match code {
+            0 => {
+                println!("Keychain updated...\n");
+                Ok(())
+            }
+            _ => Err(Error::KeyChainWriteAccess),
         }
     } else if input == "n" {
         println!("Skipped keychain update...\n");
